62aef5ef6a16de64faa23d9477d97a0f50f7dcbb
I3WX <utkarshs7828@gmail.com>
2024-09-14 12:24:12 +0530

    project compleated

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
-import "./styles.css";
+import "./style.css";
+import { greeting } from "./greeting.js";
 
-// Import images
-import blackCoffee from "./images/black coffe.jpg";
-import coldBrewCoffee from "./images/cold brew coffe.jpg";
-import coffeeWithLeaf from "./images/coffe with leaf art top pic.jpg";
-import coldCoffee from "./images/cold coffe.jpg";
-import coffeeCup from "./images/coffe cup.jpg";
-import coffeeBeans from "./images/coffe beans.jpg";
-import cafeInterior from "./images/cafe intenier.jpg";
-
-
-// Hero background image
-document.querySelector(".hero").style.backgroundImage = `url(${cafeInterior})`;
-
-// Menu images
-document.getElementById("black-coffee").src = blackCoffee;
-document.getElementById("cold-brew-coffee").src = coldBrewCoffee;
-document.getElementById("coffee-with-leaf").src = coffeeWithLeaf;
-document.getElementById("cold-coffee").src = coldCoffee;
-
-// Gallery images
-document.getElementById("coffee-cup").src = coffeeCup;
-document.getElementById("coffee-beans").src = coffeeBeans;
-document.getElementById("cafe-interior").src = cafeInterior;
+console.log(greeting);
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,41 @@
+// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  // Entry point for your application
   entry: "./src/index.js",
-
-  // Output settings
   output: {
-    filename: "bundle.js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true, // Cleans the dist folder before each build
+    clean: true,
   },
-
-  // Mode: change to 'production' when you're ready to build for production
-  mode: "development",
-
-  // Module rules for handling different file types
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    static: path.resolve(__dirname, "dist"), 
+    port: 8080, 
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
-      // CSS loader to handle CSS imports
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-      // File loader to handle images, fonts, etc.
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
     ],
   },
-
-  // Plugins
-  plugins: [
-    // This plugin generates an HTML file from a template and injects the bundle
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-      filename: "index.html",
-    }),
-  ],
-
-  // Development server configuration
-  devServer: {
-    static: "./dist",
-    port: 8080,
-    open: true, // Automatically opens the browser
-  },
-
-  // Resolve extensions so you can import files without specifying their extension
-  resolve: {
-    extensions: [".js", ".css"],
-  },
-
-  // Source map for easier debugging in development
-  devtool: "inline-source-map",
 };
